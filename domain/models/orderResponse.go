65458075ecf0d6195e8f744151a91d1ac7731106
Ибрагимов Токен <00025581@nb>
2023-10-21 23:18:09 +0600

    test

--- domain/models/orderResponse.go
+++ domain/models/orderResponse.go
@@ -3,6 +3,6 @@ package models
 type OrderResponse struct {
 	Number     string  `json:"number"`
 	Status     string  `json:"status"`
-	Accrual    float64 `json:"accrual"`
+	Accrual    float64 `json:"accrual,omitempty"`
 	UploadedAt string  `json:"uploaded_at"`
 }
